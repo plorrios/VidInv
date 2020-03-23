f1319bec2e16182ca3087867aa0febcafc9140c9
anleus <tomas.rui598@gmail.com>
2020-03-23 20:57:19 +0100

    interfaz informacion de juego usable

--- app/src/main/java/com/pabloor/vidinv/GamePageActivity.java
+++ app/src/main/java/com/pabloor/vidinv/GamePageActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class GameActivity extends AppCompatActivity {
+public class GamePageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    //llamar a setTitle de CollapsingToolbarLayout para poner el titulo que queramos, creo
 
     public void addGame(View view) {
     }
