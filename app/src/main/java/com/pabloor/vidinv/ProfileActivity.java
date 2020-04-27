004a731694daafa7b6c1bbc1b47635e413fdc5b1
Plorrios <pablolopezorrios@hotmail.es>
2020-04-27 22:37:25 +0200

    ver listas perfil

--- app/src/main/java/com/pabloor/vidinv/ProfileActivity.java
+++ app/src/main/java/com/pabloor/vidinv/ProfileActivity.java
@@ -3,19 +3,28 @@ package com.pabloor.vidinv;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.pabloor.vidinv.Objects.Game;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView nickname, completeNum, pendingNum, droppedNum, plannedNum;
+    TextView nickname, completeNum, pendingNum, droppedNum, playingNum;
     String email;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -29,7 +38,7 @@ public class ProfileActivity extends AppCompatActivity {
         completeNum = findViewById(R.id.complete_num);
         pendingNum = findViewById(R.id.pending_num);
         droppedNum = findViewById(R.id.dropped_num);
-        plannedNum = findViewById(R.id.pending_num);
+        playingNum = findViewById(R.id.playing_num);
 
         email = getIntent().getStringExtra("email");
 
@@ -54,11 +63,68 @@ public class ProfileActivity extends AppCompatActivity {
                             if (document.getLong("dropped") != null) {
                                 droppedNum.setText(document.getLong("dropped").toString());
                             } else { droppedNum.setText("0"); }
-                            if (document.getLong("planned") != null) {
-                                plannedNum.setText(document.getLong("planned").toString());
-                            } else { plannedNum.setText("0"); }
+                            if (document.getLong("playing") != null) {
+                                playingNum.setText(document.getLong("playing").toString());
+                            } else { playingNum.setText("0"); }
                         }
                     }
                 });
     }
+
+    public void OpenList(android.view.View view){
+        switch (view.getId())
+        {
+            case R.id.complete_Profile_Text:
+                getList("completed");
+                break;
+            case R.id.dropped_Profile_Text:
+                getList("dropped");
+                break;
+            case R.id.pending_Profile_Text:
+                getList("pending");
+                break;
+            case R.id.playing_Profile_Text:
+                getList("playing");
+                break;
+        }
+
+    }
+
+    public void getList(final String s){
+        final ArrayList<Game> games = new ArrayList<Game>();
+        db.collection("users").document(email).collection("games")
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            Game current;
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    if (s.equals(documentSnapshot.get("list"))) {
+                        current = new Game(Integer.parseInt(documentSnapshot.getId()), documentSnapshot.getString("name"), documentSnapshot.getString("image"));
+                        if (!checkGameInList(games, current)) {games.add(current);}
+                    }
+                }
+                ListOpen(games);
+            }
+        });
+        Log.d("count",Integer.toString(games.size()));
+
+    }
+
+    void ListOpen(ArrayList<Game> games){
+        if (!games.isEmpty()) {
+            Intent intent = new Intent(ProfileActivity.this, GameListActivity.class);
+            intent.putExtra("NAME_LIST", (ArrayList<Game>)games);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, R.string.empty_list, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkGameInList(List<Game> list, Game game) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == game.getId()) return true;
+        }
+        return false;
+    }
+
 }
